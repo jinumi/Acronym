ace508035895755799fbdee7345d0a4d12551194
Muhammad Umair Yaqub <umairyaqubofficial@gmail.com>
2023-03-05 20:05:20 +0500

    Update Acronym.py

--- Acronym.py
+++ Acronym.py
@@ -1,6 +1,18 @@
+# Designed and Developed by Muhammad Umair Yaqub.
+# Made in P A K I S T A N
+
+# Prompt the user to enter a string
 text = str(input("Enter a String\n"))
+
+# Split the input string into a list of words
 text = text.split()
+
+# Initialize an empty string to store the acronym
 acronym = ""
+
+# Loop through each word in the list and append the first character to the acronym string
 for i in text :
     acronym = acronym + str(i[0]).upper()
+
+# Print the resulting acronym
 print(acronym)
